aef91d580ec99cd93c8b72292c7bd169a81bfc5d
Chakrapani Peyyala <cpeyyala@agility.com>
2022-04-22 11:58:30 +0530

    ui changes

--- DotnetAngular/ClientApp/src/app/app.module.ts
+++ DotnetAngular/ClientApp/src/app/app.module.ts
@@ -31,7 +31,7 @@ import { FooterComponent } from './footer/footer.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
+      { path: 'fetch-data', component: FetchDataComponent},
     ])
   ],
   providers: [
